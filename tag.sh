fc52b5501cde7210e0a2b9c7e590c8d4301f0d88
harvestcore <aagomezies@gmail.com>
2020-03-19 22:30:31 +0100

    more changes v0.0.3

--- tag.sh
+++ tag.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
+echo "####################"
 echo "## Tag generation ##"
+echo "####################"
 
 tag=$(git log --pretty=oneline | head -n1 | grep -o -P "(v[0-9].[0-9].[0-9])")
 echo "Tag found: $tag"
+echo "####################"
 
 if [ "$tag" != "" ]
 then
@@ -11,10 +13,17 @@ then
   remote=$(git config --get remote.origin.url)
   repo=$(basename "$remote" .git)
 
+  echo "########################################################"
   echo "Commit: $commit"
   echo "Remote: $remote"
   echo "Repo: $repo"
+  echo "########################################################"
 
+  # curl -XPOST -H 'Authorization: token '"$GITHUB_KEY" -H "Content-type: application/json" \
+  # -d '{ "ref": "refs/heads/'"$tag"'", "sha": "'"$commit"'"}' 'https://api.github.com/repos/harvestcore/'"$repo"'/git/refs'
+
+  # echo "########################################################"
   curl -XPOST -H 'Authorization: token '"$GITHUB_KEY" -H "Content-type: application/json" \
-  -d '{ "ref": "refs/heads/'"$tag"'", "sha": "'"$commit"'"}' 'https://api.github.com/repos/harvestcore/'"$repo"'/git/refs'
+  -d '{ "tag_name": '"$tag"' }' 'https://api.github.com/repos/harvestcore/'"$repo"'/releases'
+  echo "########################################################"
 fi
