03c291cfa0bd60321d846eaa88b96eb10af2d3c0
harvestcore <aagomezies@gmail.com>
2020-03-19 22:39:57 +0100

    really final touches v0.0.5

--- tag.sh
+++ tag.sh
@@ -4,8 +4,8 @@ echo "## Tag generation ##"
 echo "####################"
 
 tag=$(git log --pretty=oneline | head -n1 | grep -o -P "(v[0-9].[0-9].[0-9])")
-echo "##   Tag: $tag    ##"
-echo "####################\n\n\n"
+echo "Tag: $tag"
+echo "####################"
 
 if [ "$tag" != "" ]
 then
@@ -13,14 +13,14 @@ then
   remote=$(git config --get remote.origin.url)
   repo=$(basename "$remote" .git)
 
-  echo "########################################################\n\n"
+  echo "########################################################"
   echo "Commit: $commit"
   echo "Remote: $remote"
   echo "Repo: $repo"
-  echo "\n\n########################################################\n\n"
+  echo "########################################################"
 
   curl -XPOST -H 'Authorization: token '"$GITHUB_KEY" -H "Content-type: application/json" \
   -d '{ "tag_name": "'"$tag"'" }' 'https://api.github.com/repos/harvestcore/'"$repo"'/releases'
 
-  echo "\n\n########################################################"
+  echo "########################################################"
 fi
