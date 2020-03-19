bb24c00bec640849d5fa5d33eb1e16ce024d8e56
harvestcore <aagomezies@gmail.com>
2020-03-19 22:38:14 +0100

    final touches v0.0.4

--- tag.sh
+++ tag.sh
@@ -4,8 +4,8 @@ echo "## Tag generation ##"
 echo "####################"
 
 tag=$(git log --pretty=oneline | head -n1 | grep -o -P "(v[0-9].[0-9].[0-9])")
-echo "Tag found: $tag"
-echo "####################"
+echo "##   Tag: $tag    ##"
+echo "####################\n\n\n"
 
 if [ "$tag" != "" ]
 then
@@ -13,17 +13,14 @@ then
   remote=$(git config --get remote.origin.url)
   repo=$(basename "$remote" .git)
 
-  echo "########################################################"
+  echo "########################################################\n\n"
   echo "Commit: $commit"
   echo "Remote: $remote"
   echo "Repo: $repo"
-  echo "########################################################"
+  echo "\n\n########################################################\n\n"
 
-  # curl -XPOST -H 'Authorization: token '"$GITHUB_KEY" -H "Content-type: application/json" \
-  # -d '{ "ref": "refs/heads/'"$tag"'", "sha": "'"$commit"'"}' 'https://api.github.com/repos/harvestcore/'"$repo"'/git/refs'
-
-  # echo "########################################################"
   curl -XPOST -H 'Authorization: token '"$GITHUB_KEY" -H "Content-type: application/json" \
-  -d '{ "tag_name": '"$tag"' }' 'https://api.github.com/repos/harvestcore/'"$repo"'/releases'
-  echo "########################################################"
+  -d '{ "tag_name": "'"$tag"'" }' 'https://api.github.com/repos/harvestcore/'"$repo"'/releases'
+
+  echo "\n\n########################################################"
 fi
